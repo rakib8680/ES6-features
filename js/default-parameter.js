da8f2ab4939e436c48df72d8b7025e44a1de9ee7
rakib8680 <rakib.cst.1st@gmail.com>
2023-02-20 15:31:42 +0600

    default parameter

--- js/default-parameter.js
+++ js/default-parameter.js
@@ -1,2 +1,12 @@
 
 
+
+// you can set your parameter value 
+function calculateNum(value1 = 0, value2 = 5) {
+    const formula = value1 + value2;
+    return formula;
+}
+
+// const result = calculateNum(10);
+const result = calculateNum();
+console.log(result);
