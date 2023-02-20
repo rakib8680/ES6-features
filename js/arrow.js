92e48288ef9608514c1092b2fdcd85c06d2c6c20
rakib8680 <rakib.cst.1st@gmail.com>
2023-02-20 18:50:42 +0600

    Arrow Function

---
+++ js/arrow.js
@@ -0,0 +1,22 @@
+
+
+
+// function expression 
+// anonymous function 
+const calculation = function (num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+// console.log(calculation(5,6));
+
+
+
+
+// arrow function 
+const add = (first, second) => first + second;
+
+console.log(add(3, 30));
+
+// ** Interview question ** 
+// Difference between arrow function and function declaration and function expression 
