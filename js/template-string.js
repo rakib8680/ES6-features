24fca68ea8be07c052beefba2eb39699daa25e58
rakib8680 <rakib.cst.1st@gmail.com>
2023-02-20 15:30:31 +0600

    first commit

---
+++ js/template-string.js
@@ -0,0 +1,26 @@
+
+
+const name1 = 'rakiib';
+const name2 = "Khan";
+
+// For multiline string use this ( ` ` ) 
+
+const name3 = `Devid Beckham
+Rakib Khan
+leonel Messi
+cristiano ronaldo
+neymar junior
+kevin debruyne
+gullit
+rijkard
+van vasten
+`;
+// console.log(name3);
+
+
+
+// Use ${ }  to show expressions inside ( ` `) this 
+
+const num1 = 5;
+const num2 = 64;
+console.log(`The sum of ${num1} and ${num2} is: ${num1 + num2}`);
