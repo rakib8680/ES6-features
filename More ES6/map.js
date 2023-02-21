16c80be28eec5950a70fd7b2de2f0db746e92a73
rakib8680 <rakib.cst.1st@gmail.com>
2023-02-21 13:53:34 +0600

    Map array

--- More ES6/map.js
+++ More ES6/map.js
@@ -5,4 +5,10 @@ const number = [3, 54, 6, 2, 67];
 // const makeItDouble = x => x * 2;
 
 const doubleIt = number.map(x => x * 2);
-console.log(doubleIt);
+// console.log(doubleIt);
+
+
+
+
+const makeFiveTimes = [3, 1, 5, 5, 7, 2, 8, 6].map(x => x * 5);
+console.log(makeFiveTimes);
