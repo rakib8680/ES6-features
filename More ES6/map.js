2308e7eafe796cedf86b99a1b6abb312541a75ec
rakib8680 <rakib.cst.1st@gmail.com>
2023-02-21 14:49:28 +0600

    mapping

--- More ES6/map.js
+++ More ES6/map.js
@@ -8,7 +8,27 @@ const doubleIt = number.map(x => x * 2);
 // console.log(doubleIt);
 
 
-
-
 const makeFiveTimes = [3, 1, 5, 5, 7, 2, 8, 6].map(x => x * 5);
-console.log(makeFiveTimes);
+// console.log(makeFiveTimes);
+
+
+
+const friends = ['Rakib', 'Kuddus', 'BatMan', 'Kang'];
+
+const firstChar = friends.map(n => n[0]);
+const nameLength = friends.map(k => k.length);
+
+// console.log(` ${firstChar}  
+//               ${nameLength}  `);
+
+
+
+
+const products = [
+    { id: 1, name: 'laptop', price: 344444 },
+    { id: 2, name: 'iphone', price: 144444 },
+    { id: 1, name: 'tablet', price: 44444 }
+];
+
+const price = products.map(product => product.price);
+console.log(price);
