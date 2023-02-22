efb9ee6177e52facf730024941fb5f844fa67726
rakib8680 <rakib.cst.1st@gmail.com>
2023-02-22 17:34:32 +0600

    Destructuring Arraay

---
+++ ES6-Recap/spreadArray.js
@@ -0,0 +1,16 @@
+
+
+const ages = [33, 13, 35, 55, 34, 11, 46, 26, 23];
+const maxAge = Math.max(...ages);
+const minAge = Math.min(...ages);
+// console.log(`Maximum age is : ${maxAge} and Minimum age is : ${minAge}`);
+
+const numbers = [3, 5, 2, 8, 3, 5, 8, 5, 9, 6, 8];
+const agesPlusNumbers = [...ages, ...numbers];
+// console.log(agesPlusNumbers);
+
+
+
+
+
+
