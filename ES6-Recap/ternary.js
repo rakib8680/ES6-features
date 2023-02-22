1bd1905407539970ee65fbb049427c9378555646
rakib8680 <rakib.cst.1st@gmail.com>
2023-02-22 17:08:55 +0600

    ternary

---
+++ ES6-Recap/ternary.js
@@ -0,0 +1,26 @@
+
+// Use Ternary procedure , when you have to take only Two decision  
+
+
+let num = 23;
+
+
+// normal procedure 
+let result = '';
+if (num == 23) {
+    result = 'The result is true';
+}
+else {
+    result = 'The result is false';
+}
+// console.log(result);
+
+
+
+// Ternary procedure 
+// 1
+// (num === 24) ? console.log('True') : console.log('False');
+
+// 2
+result = (num == 23) ? 'This is true' : 'This is false'
+// console.log(result);
